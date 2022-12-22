abdd0d1289572ca52a1da7ed50d73350841bbff7
Michael Bialowas <mbialowas@rrc.ca>
2022-12-22 11:29:19 -0600

    Added Movies and ProductionCompanies controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "csv"
+
+Movie.delete_all
+ProductionCompany.delete_all
+
+# open top_movies.csv
+# the path to the root folder is: Rails.root
+filename = Rails.root.join("db/top_movies.csv")
+
+puts "Loading Movies from the CSV file: #{filename}"
+
+csv_data = File.read(filename)
+movies = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+movies.each do |m|
+  production_company = ProductionCompany.find_or_create_by(name: m["production_company"])
+
+  if production_company && production_company.valid?
+    # create the movie!
+    movie = production_company.movies.create(
+      title:        m["original_title"],
+      year:         m["year"],
+      duration:     m["duration"],
+      description:  m["description"],
+      average_vote: m["avg_vote"]
+    )
+    puts "Invalid movie #{m['original_title']}" unless movie&.valid?
+  else
+    puts "invalid production company #{m['production_company']} for movie #{m['original_title']}."
+  end
+end
+puts "Created #{ProductionCompany.count} Production Companies"
+puts "Created #{Movie.count} movies."
